fed74768ae0cac06c9ebbc8a5de0b4d9dcc62592
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-04-14 21:54:18 +0300

    01-linux-arch: Determine page size (skeleton)

---
+++ 01-linux-architecture/page-size/page-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "util.h"
+
+int main(void)
+{
+	/* TODO: Call getpagesize() to find out page size. */
+
+	/* TODO: Call sysconf() to find out page size. */
+
+	return 0;
+}
