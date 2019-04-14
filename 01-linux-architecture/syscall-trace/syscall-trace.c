a8c7c85df2afb27b657c83002512c1aab5a95044
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2019-04-14 21:55:55 +0300

    01-linux-arch: Trace system calls for libc calls (skeleton)

---
+++ 01-linux-architecture/syscall-trace/syscall-trace.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	/* TODO: Call sleep() to trace system call. */
+
+	/* TODO: Call system() to trace system call. */
+
+	/* TODO: Call strdup() to trace system call. */
+
+	/* TODO: Call strcpy() to trace system call. */
+
+	return 0;
+}
